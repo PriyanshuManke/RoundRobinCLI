14bd23678f44c695a5b868209ace3fdce248117e
priyanshu manke <mankepriyanshu@gmail.com>
2022-02-07 20:53:14 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundRobinCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPlayers, numGroup;
+
+            Console.WriteLine("Enter number of Players in each group ");
+            numPlayers = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number of Groups ");
+            numGroup = Convert.ToInt32(Console.ReadLine());
+
+            if ((numGroup == 0))
+                 Console.WriteLine("Number of Groups must be greater than 0");
+ 
+            if ((numPlayers == 0 || numPlayers < 2) && (numGroup == 0))
+                 Console.WriteLine("Number of Players must be greater than 1");
+             
+            else
+            {
+                int xx = 0;
+
+                for (int m = 0; m < numGroup; m++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Group " + (m + 1) + " Fixture");
+                    Console.WriteLine();
+
+                    List<string> teams = new List<string> { };
+
+                    for (int i = 0; i < numPlayers; i++)
+                    {
+                        xx += 1;
+
+                        teams.Add("Player " + xx);
+                    }
+
+                    //List<string> teams = new List<string> { "Chennai Super", "RCB", "Mumbai Indians", "Delhi", "KKR", "RR" };
+
+                    foreach (var match in RoundRobin.ListMatches(teams))
+                    {
+                        Console.WriteLine($"{(match.Day + 1) + " day"} => {match.First}-{match.Second}");
+                    }
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ RoundRobin.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic; //not specific to a particular datatype.
+using System.Linq;  //set of technologies based on the integration of query.
+
+namespace RoundRobinCLI
+{
+    internal class RoundRobin
+    {
+        //IEnumerable = interface that define one method.
+        //IList = interface that represent a collection of object.
+        public static IEnumerable<(int Day, L First, L Second)> ListMatches<L>(IList<L> teams)
+        {
+            var matches = new List<(int, L, L)>();
+
+            if (teams == null || teams.Count < 2)
+                return matches;
+
+            //the default value of the teams is 0 therefore we write Skip(1).
+            var restTeams = new List<L>(teams.Skip(1));
+            var teamsCount = teams.Count;
+
+            if (teams.Count % 2 != 0)
+            {
+                restTeams.Add(default);
+                teamsCount++;
+            }
+
+            for (var day = 0; day < teamsCount - 1; day++)
+            {
+                if (restTeams[day % restTeams.Count]?.Equals(default) == false)
+                    matches.Add((day, teams[0], restTeams[day % restTeams.Count]));
+
+                //index = allow instane of class to be indexed just like array.
+                for (var index = 1; index < teamsCount / 2; index++)
+                {
+                    var firstTeam = restTeams[(day + index) % restTeams.Count];
+                    var secondTeam = restTeams[(day + restTeams.Count - index) % restTeams.Count];
+                
+                    if (firstTeam?.Equals(default) == false && secondTeam?.Equals(default) == false)
+                        matches.Add((day, firstTeam, secondTeam));
+ 
+                }
+            }
+
+            return matches;
+        }
+    }
+
+}
